c0fd1d40f0e3545d5f73c5c8db29cedf159c60ea
Kevin Sacuedo <user002_@outlook.com>
2026-09-21 11:06:41 -0500

    add default private route

--- httpserver/handlers.go
+++ httpserver/handlers.go
@@ -6,23 +6,35 @@ import (
 	"github.com/labstack/echo/v5"
 )
 
+func getMethod(method string) string {
+	if method == "" {
+		return http.MethodGet
+	}
+	return method
+}
+
+func handleRequest(handler echo.HandlerFunc, c *echo.Context) error {
+	if handler == nil {
+		return echo.NewHTTPError(
+			http.StatusInternalServerError,
+			"route handler is not configured",
+		)
+	}
+	return handler(c)
+}
+
 // Public
 type PublicHandler struct {
-	Method           string
-	Path             string
-	RequiredPerms    []string
-	AnyRequiredPerms []string
-	Handler          echo.HandlerFunc
+	Method  string
+	Path    string
+	Handler echo.HandlerFunc
 }
 
 var _ Route = (*PublicHandler)(nil)
 
 // GetMethod implements [Route].
 func (h *PublicHandler) GetMethod() string {
-	if h.Method == "" {
-		return http.MethodGet
-	}
-	return h.Method
+	return getMethod(h.Method)
 }
 
 // GetPath implements [Route].
@@ -32,11 +44,29 @@ func (h *PublicHandler) GetPath() string {
 
 // HandleRequest implements [Route].
 func (h *PublicHandler) HandleRequest(c *echo.Context) error {
-	if h.Handler == nil {
-		return echo.NewHTTPError(
-			http.StatusInternalServerError,
-			"route handler is not configured",
-		)
-	}
-	return h.Handler(c)
+	return handleRequest(h.Handler, c)
+}
+
+// Private
+type PrivateHandler struct {
+	Method  string
+	Path    string
+	Handler echo.HandlerFunc
+}
+
+var _ Route = (*PrivateHandler)(nil)
+
+// GetMethod implements [Route].
+func (h *PrivateHandler) GetMethod() string {
+	return getMethod(h.Method)
+}
+
+// GetPath implements [Route].
+func (h *PrivateHandler) GetPath() string {
+	return h.Path
+}
+
+// HandleRequest implements [Route].
+func (h *PrivateHandler) HandleRequest(c *echo.Context) error {
+	return handleRequest(h.Handler, c)
 }
